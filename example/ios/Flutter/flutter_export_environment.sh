17f1079783ff6770719fcde971ef0131bd94c9fc
yinxing <yinxing29@sina.com>
2024-02-28 00:39:08 +0800

    修改

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yinxing/Documents/FlutterSDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yinxing/Desktop/flutter_library/flutter_tableview/example"
+export "FLUTTER_ROOT=/Users/yinxing/Documents/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yinxing/Desktop/flutter_project/flutter_tableview_copy/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
